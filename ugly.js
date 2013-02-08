9e4487854d1cd09c4a1a7efe0aa6c54e541b5f01
Joshua Theze <fox@foxwolfblood.me>
2013-02-07 20:01:16 -0500

    added the ugly.js file

---
+++ ugly.js
@@ -0,0 +1,14 @@
+// ugly.js 
+// this is probably the worst thing you can do to your users
+// it uglifies a site with a call to uglify() and contains many functions to
+// add to the ugly in your site programming.
+// Lets bring back the 90s web design style with our new stuff!
+
+
+// Contributers
+// Joshua Theze (Foxiepaws) - joshua.theze@gmail.com
+
+// just the stub that makes everything horrible.
+function uglify() {
+
+}
