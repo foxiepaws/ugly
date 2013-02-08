7d67988e71ae7d4a26ab8e91009ff8c67007793c
Nicholas Alonge <nick.alonge@Gmail.com>
2013-02-07 17:13:53 -0800

    added function get_random_color()

--- ugly.js
+++ ugly.js
@@ -12,3 +12,14 @@
 function uglify() {
 
 }
+
+/* get_random_color()
+returns a random hexidecimal color value */
+function get_random_color() {
+  var letters = '123456789ABCDEF'.split('');
+  var color="#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.round(Math.random() * 15)];
+  }
+  return color;
+}
